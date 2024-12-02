d1f5fb025b489884c205ffcc88bdba9671386144
falclop <falclop@demo.es>
2024-12-02 12:50:43 +0000

    a

---
+++ scripts/setup_nfs_client.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#mostrar los comandos que se van ejecutando
+#la e es para parar la ejecución si falla, la x para mostrar el comando
+#Cogemos las variables
+source .env
+#Vamos a crear un proxy inverso
+set -ex
+
+#Actualizar el repositorio
+apt update
+
+#Actualiza el paquete
+apt upgrade -y
+
+sudo apt install nfs-common -y
+
+#Montamos la carpeta en el directorio, con df -h podemos comprobarlo
+mount $NFS_SERVER_IP:/var/www/html /var/www/html
